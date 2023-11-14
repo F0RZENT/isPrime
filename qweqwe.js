4a2372c6ef865715bdc60abc311ff97e6f19f8e1
F0RZENT <113331099+F0RZENT@users.noreply.github.com>
2023-11-14 12:59:01 +0500

    Add files via upload

---
+++ qweqwe.js
@@ -0,0 +1,21 @@
+function isPrime(x){
+    if(x <= 1){
+        return false;
+    }
+    for (let i = 2; i < x; i++) {
+        if (x % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+isPrime(28);
+function getPrimes(limit) {
+    let i = 2;
+    while(i <= limit){
+        for(let i = 2; i < limit; i++){
+            
+        }
+    }
+    return
+}
